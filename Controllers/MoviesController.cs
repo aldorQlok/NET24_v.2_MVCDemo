f5392d2ac1df951c4b3368f9e297119eafc0763a
aldorQlok <aldor.besher@qlok.se>
2025-08-27 12:57:11 +0200

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieMVCDemo.Models;
+
+namespace MovieMVCDemo.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Simulerar att jag gör API-anrop eller Hämtar data från en databas...
+            var movies = new List<Movie>()
+            {
+                new Movie { Id = 1, Title = "Movie 1", Genre = "Genre 1", ReleaseYear = 2015},
+                new Movie { Id = 2, Title = "Movie 2", Genre = "Genre 2", ReleaseYear = 2016},
+                new Movie { Id = 3, Title = "Movie 3", Genre = "Genre 3", ReleaseYear = 2017},
+            };
+
+            return View(movies);
+        }
+    }
+}
